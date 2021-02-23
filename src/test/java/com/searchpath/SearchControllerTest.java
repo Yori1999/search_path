b5ba5930638629ff794ed0465fc03aaefd6986aa
Yori1999 <mariaf987@gmail.com>
2021-02-23 13:23:35 +0100

    Added test for search

--- src/test/java/com/searchpath/SearchControllerTest.java
+++ src/test/java/com/searchpath/SearchControllerTest.java
@@ -18,10 +18,21 @@ public class SearchControllerTest {
 
     @Test
     public void testSearch() {
+        //With no query parameter
         HttpRequest<String> request = HttpRequest.GET("/search");
         String body = client.toBlocking().retrieve(request);
         Assertions.assertNotNull(body);
-        //Assertions.assertEquals(body);
+        Assertions.assertEquals("{\"cluster_name\":\"docker-cluster\"}", body);
+        //With 1 word query parameter
+        request = HttpRequest.GET("/search?query=shoes");
+        body = client.toBlocking().retrieve(request);
+        Assertions.assertNotNull(body);
+        Assertions.assertEquals("{\"query\":\"shoes\",\"cluster_name\":\"docker-cluster\"}", body);
+        //With several words query parameter
+        request = HttpRequest.GET("/search?query=black%20shoes"); //%20 used for concatenating words that are separated by blank spaces
+        body = client.toBlocking().retrieve(request);
+        Assertions.assertNotNull(body);
+        Assertions.assertEquals("{\"query\":\"black shoes\",\"cluster_name\":\"docker-cluster\"}", body);
     }
 
 }
