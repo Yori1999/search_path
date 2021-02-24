30e0edd9426b28ce59941bff5b1267ed90ca220a
Yori1999 <mariaf987@gmail.com>
2021-02-24 10:47:42 +0100

    Privatiziced fields + added corresponding getters

--- src/main/java/com/searchpath/entities/Message.java
+++ src/main/java/com/searchpath/entities/Message.java
@@ -1,11 +1,21 @@
 package com.searchpath.entities;
 
 public class Message {
-    public String query, cluster_name;
+    private String query, cluster_name;
 
     public Message(String query, String cluster_name){
         this.query = query;
         this.cluster_name = cluster_name;
     }
+
+    public String getQuery(){
+        return query;
+    }
+
+    public String getCluster_name(){
+        return cluster_name;
+    }
+
+
 
 }
